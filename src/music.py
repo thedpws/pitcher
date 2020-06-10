0229ff1e2092b47834244c8e3ee95b71df565ba3
AZ Vasquez <azvasquez99@gmail.com>
2020-06-10 15:26:05 -0400

    Make script runnable

--- src/music.py
+++ src/music.py
@@ -1,84 +1,58 @@
 
-'''
+from enum import Enum
+
+class Key(Enum):
+    C_MAJOR = 0
 
 
+class Time(Enum):
+    COMMON_TIME = 0
 
-Design decisions:
+class Clef(Enum):
+    TREBLE = 0
 
-
-Piece > Parts 
-Part > Staves
-
-Part > tempo, time/key signature, staves, brace/bracket 
-
-N-voice staff > measures, clef, instrument/voice
-
-Measure > Notes/Rests, N lines, technique (forte, legato, etc)
-
-Note > pitch, duration
-
-pitch > octave (c3, c4, c5), accidental
-
-1. Parts - Parts are necessary because it is not uncommon to switch tempo, key signatures, vocal arrangements, etc.
-
-
-2. Indexing - 
-    * You should not be able to append a single note to a DS expecting multiple notes (1-voice staff vs 2-voice staff).
-
-    * Indexing a structure accesses its substructures. (In contrast to accessing notes)
-
-    * Notes cannot be indexed using integers since they may appear at half counts.
-
-2. A System is not a DS. It is simply a side effect of how the program decides to format the sheet music. But users should be able to define how many measures are in each system.
-
-
-4. Reserved keywords
-    * Section (Brass section)
-    * 
-
-'''
+class Voice(Enum):
+    PIANO = 0
 
 class _Music:
 
     def listen(self, **kwargs):
-        '''Should play music'''
+        '''Plays music'''
         raise NotImplementedError
 
     def show(self, **kwargs):
-        '''Should show a graphic of music'''
+        '''Shows a graphic of music'''
         raise NotImplementedError
 
     def save(self, filename, **kwargs):
-        '''Should save music to a PDF'''
+        '''Saves music to a PDF'''
         raise NotImplementedError
 
 class Score(_Music):
-    '''Contains textual information and optionally arguments for the first Part'''
+    '''Contains textual information and optional arguments for the first Part'''
+    def __init__(self, title=None, subtitle=None, key_signature=None, time_signature=None):
+        raise NotImplementedError
 
-    def __init__(self, title=None, subtitle=None):
-        self.parts = [Part()]
 
-
-# Will handle things like repeats, lyrics, 
-# How to group into sections (bracket)?
 class Part(_Music):
-    '''A collection of staffs'''
-
-    @key_signature.setter
-    def (self, key_signature):
-        pass
+    '''A collection of staffs. Add effects / stanza-chorus / key/time changes to parts. Should affect its children.'''
 
     @property
     def key_signature(self):
-        pass
+        raise NotImplementedError
 
-    @time_signature.setter
-    def (self, time_signature):
-        pass
+    @key_signature.setter
+    def key_signature(self, key_signature):
+        raise NotImplementedError
+
 
     @property
     def time_signature(self):
-        pass
+        raise NotImplementedError
+
+    @time_signature.setter
+    def time_signature(self, time_signature):
+        raise NotImplementedError
 
 
     def __init__(
@@ -87,7 +61,7 @@ class Part(_Music):
             time_signature=Time.COMMON_TIME,
             key_signature=Key.C_MAJOR,
     ):
-        pass
+        raise NotImplementedError
 
 class _Staff(_Music):
 
@@ -99,7 +73,7 @@ class _Staff(_Music):
         self._measures = []
 
 class VoicedStaff(_Staff):
-    '''A staff with distinct voicing'''
+    '''A staff with distinct voicing (SATB)'''
     '''Notes must be attached to a specific voice'''
 
     def __init__(self, clef=Clef.TREBLE, voice=Voice.PIANO):
@@ -109,16 +83,23 @@ class VoicedStaff(_Staff):
         return self._voices[voice]
 
 class UnvoicedStaff(_Staff):
-    '''A staff without distinct voicing'''
+    '''A staff without distinct voicing (Piano pieces)'''
     '''Notes are not attached to voices'''
-    pass
+    def __init__(self, clef=Clef.TREBLE, voice=Voice.PIANO):
+        raise NotImplementedError
 
+# TODO: Bind measure length by global time signature
+class Measure(_Music):
+    '''Collection of notes'''
 
-# : How to keep all durations rational wrt the time signature?
-# TODO: Tremolo
+    def __init__(self, notes=None):
+        if not notes:
+            notes = []
+        self._notes = notes
 
+# TODO: Add effects
 class Note(_Music):
-    '''Has pitch and duration. Also accidentals.'''
+    '''Has pitch and duration. Also accidentals and note-effects (tremolo)'''
     def __init__(self, pitch, duration):
         self._pitch = pitch
         self._duration = duration
@@ -127,16 +108,3 @@ class Rest(Note):
     '''Has no pitch. Only duration.'''
     def __init__(self, duration):
         super(Rest, self).__init__(pitch=None, duration=duration)
-
-
-
-        Piece line issue
-        
-pitcher.CMajor()
-
-Measure()
-
-# TODO: Look for music dataset
-# TODO: Performance angle w/ python lib?
-# TODO: Global time signatures
-# TODO: Look at Mingus
