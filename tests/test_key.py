a61b9586c23ff6fea5ea9e9e00cc39cd7bdec287
AZ Vasquez <azvasquez99@gmail.com>
2020-06-30 16:14:56 -0400

    Beginning music test files

---
+++ tests/test_key.py
@@ -0,0 +1,61 @@
+
+
+import unittest
+from pitchr import *
+
+class TestKey(unittest.TestCase):
+
+    def test_key_fails_on_bad_key_signature_both_flats_and_sharps(self):
+        act = lambda: Key(flats=1, sharps=1)
+
+        self.assertRaises(PitcherException, act)
+
+    def test_key_fails_more_than_7_flats(self):
+        act = lambda: Key(flats=8)
+        self.assertRaises(PitcherException, act)
+
+    def test_key_fails_more_than_7_sharps(self):
+        act = lambda: Key(sharps=8)
+        self.assertRaises(PitcherException, act)
+
+    def test_key_fails_on_less_than_0_flats(self):
+        act = lambda: Key(flats=-1)
+        self.assertRaises(PitcherException, act)
+
+    def test_key_fails_on_less_than_0_sharps(self):
+        act = lambda: Key(sharps=-1)
+        self.assertRaises(PitcherException, act)
+        
+    def test_key_allows_good_key_signatures(self):
+
+        acts = [lambda: Key(*args) for args in [(0,0), (1, 0), (0,1)]]
+
+        for act in acts:
+            try:
+                act()
+            except Exception:
+                self.fail(f'{act} raised an Exception')
+    
+    def test_key_str_c_major(self):
+
+        k = str(Key.C_MAJOR)
+
+        self.assertEqual(k, 'c \\major')
+
+    def test_key_str_c_minor(self):
+
+        k = str(Key.C_MINOR)
+
+        self.assertEqual(k, 'c \\minor')
+
+    def test_key_str_c_sharp_major(self):
+
+        k = str(Key.C_SHARP_MAJOR)
+
+        self.assertEqual(k, 'c-sharp \\major')
+
+    def test_key_str_e_flat_major(self):
+
+        k = str(Key.Eb_MAJOR)
+
+        self.assertEqual(k, 'e-flat \\major')
---
+++ tests/test_score.py
@@ -0,0 +1,75 @@
+
+
+import unittest
+import tempfile, os
+from unittest.mock import patch
+from pitchr import *
+
+class TestScore(unittest.TestCase):
+    
+    def test_composer_is_property(self):
+        s = Score(author='AZ Vasquez')
+        
+        composer = s.composer
+
+        self.assertEqual(composer, 'AZ Vasquez')
+
+        def act():
+            s.composer = 'AZ Velasquez'
+        self.assertRaises(Exception, act)
+
+    def test_author_is_property(self):
+        s = Score(author='AZ Vasquez')
+        
+        author = s.author
+
+        self.assertEqual(author, 'AZ Vasquez')
+
+        def act():
+            s.author = 'AZ Velasquez'
+        self.assertRaises(Exception, act)
+
+    def test_title_is_property(self):
+        s = Score(title='AZ Vasquez')
+        
+        title = s.title
+
+        self.assertEqual(title, 'AZ Vasquez')
+
+        def act():
+            s.title = 'AZ Velasquez'
+        self.assertRaises(Exception, act)
+
+    def test_add_parts_adds_part(self):
+        s = Score()
+        s.add_part(p := Part())
+
+        self.assertEqual(s[0], p)
+
+    def test_init_parts_passed_adds_part(self):
+        s = Score(parts=[p := Part()])
+
+        self.assertEqual(s[0], p)
+
+    @patch('pitchr.playing.play_score')
+    def test_play_calls_play_score(self, play_score):
+        s = Score()
+        s.play()
+
+        self.assertTrue(play_score.called)
+
+    @patch('pitchr.lyexport.show_score_png')
+    def test_show_calls_show_score(self, show_score_png):
+        s = Score()
+        s.show()
+
+        self.assertTrue(show_score_png.called)
+
+    def test_save_creates_pdf(self):
+        s = Score()
+        with tempfile.TemporaryDirectory() as tempdirname:
+
+            filepath = tempdirname + '/export.pdf'
+            s.save(filepath)
+            self.assertTrue(os.path.exists(filepath))
+
