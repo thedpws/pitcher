c34c87915ddce418894c51f57031d686a6b9e7e4
AZ Vasquez <azvasquez99@gmail.com>
2020-07-31 02:05:41 -0400

    Fix to account for non-1.0 durations

--- pitchr/df_import.py
+++ pitchr/df_import.py
@@ -2,29 +2,36 @@
 from pitchr.music import Score, Part, Measure, Note, time, Time
 
 def measures_from_dataframe(harmony_df, durations, time_signature):
+    """Transform a dataframe of Pitch Numbers into a list of measures
+
+    :param harmony_df: Dataframe with column 'Pitch Number'
+    :param durations: a list of float detailing corresponding durations of each row in harmony_df
+    :param time_signature: string. Example: '4/4'
+
+    """
 
     time(Time(time_signature))
-
-    # Zip df with durations
 
     harmony_df['Duration'] = durations
 
     notes = list(harmony_df.apply(lambda row: Note(int(row['Pitch Number']), float(row['Duration'])), axis=1))
 
-    beat_count, beat_type = map(int, time_signature.partition('/')[::2])
-
     time(Time(time_signature))
 
     measures = []
 
-    for i in range(len(notes) // beat_count):
-        start = i * beat_count
-        end = min((i + 1) * beat_count, len(notes))
-        measure_notes = notes[start:end]
-        measures.append(Measure(measure_notes))
+    m = Measure()
 
+    while notes:
+        try:
+            print(notes[0])
+            m.append(notes[0])
+            notes.pop(0)
+        except Exception:
+            measures.append(m)
+            m = Measure()
 
-
+    measures.append(m)
 
 
     return measures
